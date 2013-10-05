542fd20553cba1730ffa6773a581c9b056ed69f1
msilverblatt <msilverblatt@gmail.com>
2013-10-05 17:36:25 -0400

    broken house form

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -6,10 +6,26 @@ class HouseController < ApplicationController
   def show
     @house = House.find(params[:id])
     @users = @house.users
-#    @users = User.where(:house_id => @house.id)
+    @bills = @house.bills
+    @payments = @house.payments
+  end
+
+  def new
+    @house = House.new
   end
 
   def create
+    @house = House.new(house_params)
+
+    respond_to do |format|
+      if @house.save
+        format.html { redirect_to @house, notice: 'House was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @house }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @house.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
--- app/models/house.rb
+++ app/models/house.rb
@@ -2,4 +2,5 @@ class House < ActiveRecord::Base
 	validates_presence_of :name
 	has_many :users
 	has_many :bills
+	has_many :payments
 end
