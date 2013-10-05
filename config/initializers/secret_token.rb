b73e67410d49ea592b805f059fac8072c764bb62
msilverblatt <msilverblatt@gmail.com>
2013-10-05 12:21:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rentsplitter::Application.config.secret_key_base = '6b114376c8bf6a9b879ffc59ff40b7453ccac35f894c340c1fc846a120ae8f136644df8b60eff23fa07bc1f73d7dd980b77cc768727723ab5d9b4c01ff8050bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rentsplitter::Application.config.session_store :cookie_store, key: '_rentsplitter_session'
